84da63cc98634cf6e3e7bebcdaf4f2fe90695c6e
elisangela94 <110443808+elisangela94@users.noreply.github.com>
2023-04-09 12:09:37 -0300

    add exercises of python

---
+++ ex005.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite um número:"))
+a = n1 - 1
+s = n1 + 1
+print("O antecessor deste número é {}, e o sucessor é {} ".format(a, s))
---
+++ ex010.py
@@ -0,0 +1,2 @@
+n1 = int(input('qual valor será convertido?'))
+print("Com {}$ você pode comprar {:.2f} dollares".format(n1, n1/5.22))
