f5d984adf3551464d96ac65d4227a143aa346312
elisangela94 <110443808+elisangela94@users.noreply.github.com>
2023-04-09 12:40:47 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input("digite um número: "))
-d = n1 + n1
-t = d + n1
-r = pow(n1, (0.5))
-print("o dobro deste valor é {}, \no triplo deste valor é {}, \ne a raiz quadrada é {:.2f}".format(d, t, r))
