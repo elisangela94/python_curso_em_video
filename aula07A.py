7e0b8d09e99cd33e22790eb76334fc63668496c5
elisangela94 <110443808+elisangela94@users.noreply.github.com>
2023-04-09 12:51:03 -0300

    Delete aula07A.py

--- aula07A.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("digite um valor: "))
-n2 = int(input("Digite outro valor: "))
-s = n1 + n2
-m = n1 * n2 
-d = n1 / n2
-d1 = n1 // n2
-e = n1 ** n2
-print("  A soma é {},\n e o produto é {}, \n e a divisão é {}".format(s, m, d))
-print("a divisão inteira é {}, \n e a potência é {}".format(d1, e))
